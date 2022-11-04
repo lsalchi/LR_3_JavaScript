fedb3e31e92cd9529a4522dea2929a227791fc21
lsalchi <115349652+lsalchi@users.noreply.github.com>
2022-11-04 18:03:48 +0300

    Update Task6.js

--- Task6.js
+++ Task6.js
@@ -1,13 +1,14 @@
-function findVowels(str)
-{
-    let res=0
-    res+=str.split('').filter(str => str=='a').length;
-    res+=str.split('').filter(str => str=='e').length;
-    res+=str.split('').filter(str => str=='i').length;
-    res+=str.split('').filter(str => str=='o').length;
-    res+=str.split('').filter(str => str=='u').length;
-    return res
-}
+const findVowels = str => {
+    let count = 0
+    const vowels = ['a', 'e', 'i', 'o', 'u']
+    for(let char of str.toLowerCase()) {
+//метод includes() выявляет содержит ли массив определенное значение, возвращает true, если массив содержит указанное значение, и false, если нет.
+      if(vowels.includes(char)) {
+        count++
+      }
+    }
+    return count
+  }
 console.log(findVowels('hello,it is me'));
 console.log(findVowels('i was wondering if after all these years'));
 console.log(findVowels('you would like to meet, to go over'));
